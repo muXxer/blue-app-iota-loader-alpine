5d7ff7a6481ff9eb3b695a6ff6fed23ec1b9c970
muXxer <mux3r@web.de>
2018-05-28 01:29:05 +0200

    Add verifying of digital certificate and polling of Ledger

--- install_alpine_environment.sh
+++ install_alpine_environment.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-apt update
-apk add --no-cache curl python3 python3-dev gcc git autoconf pkgconf musl-dev libffi-dev automake libtool libusb-dev eudev-dev linux-headers zlib-dev jpeg-dev
+apk update
+apk add --no-cache gnupg curl python3 python3-dev gcc git autoconf pkgconf musl-dev libffi-dev automake libtool libusb-dev eudev-dev linux-headers zlib-dev jpeg-dev
+gpg --import public.asc
 python3 -m ensurepip
-rm -r /usr/lib/python*/ensurepip
 pip3 install --upgrade pip setuptools wheel
-if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi
-if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi
-rm -r /root/.cache
 pip3 install git+https://github.com/LedgerHQ/blue-loader-python.git
-chmod +x iota_loader
 cp iota_loader /etc/init.d/
 rc-update add iota_loader
+apk del --purge python3-dev gcc git autoconf pkgconf musl-dev libffi-dev automake libtool eudev-dev linux-headers zlib-dev jpeg-dev
+rm -r /root/.cache
