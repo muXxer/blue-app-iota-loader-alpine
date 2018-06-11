ba58fa75e70e0c2e522dcd4993468dce4ff93ddf
muXxer <mux3r@web.de>
2018-06-12 01:08:24 +0200

    Add ncurses because of tput

--- install_alpine_environment.sh
+++ install_alpine_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apk update
-apk add --no-cache gnupg curl python3 python3-dev gcc git autoconf pkgconf musl-dev libffi-dev automake libtool libusb-dev eudev-dev linux-headers zlib-dev jpeg-dev
+apk add --no-cache ncurses gnupg curl python3 python3-dev gcc git autoconf pkgconf musl-dev libffi-dev automake libtool libusb-dev eudev-dev linux-headers zlib-dev jpeg-dev
 gpg --import public.asc
 python3 -m ensurepip
 pip3 install --upgrade pip setuptools wheel
